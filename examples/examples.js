df2627cbfece2964e966899d5be30c06c86f4ad4
Dennis Bunfield <dbunfield@aegistg.com>
2018-12-22 23:47:49 -0600

    added examples on how to create identity addresses and chains.

--- examples/examples.js
+++ examples/examples.js
@@ -1,5 +1,12 @@
 var bip44 = require('../index.js')
 const fctUtils = require('factom/src/addresses')
+const fctUtilsId = require('factom-vote/src/factom-identity')
+const { Entry } = require('factom/src/entry')
+const sign = require('tweetnacl/nacl-fast').sign
+const { add } = require('factom/src/add')
+const { commitChain, revealChain } = require('factom/src/add')
+
+const { Chain, composeChainCommit, composeChainReveal, composeChain, composeChainLedger, computeChainTxId } = require('factom/src/chain')
 
 // Mnemonic seed
 var mn = 'yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow'
@@ -8,21 +15,83 @@ var mn = 'yellow yellow yellow yellow yellow yellow yellow yellow yellow yellow 
 // Create the wallet
 var wallet = new bip44.FactomBIP44(mn)
 
-if ( bip44.validMnemonic(mn) ) console.log("VALID!!!!!!!!!!!!!!!!!!!!!!!")
-else console.log("NOT VALID!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+if ( bip44.validMnemonic(mn) ) console.log("Valid Mneumonic!")
+else console.log("Invalid Mneumonic!")
 
 // Generating private keys. Typically you only need to increment the last parameter,
 // unless you want to make multiple chains of addresses
 var privKey1 = wallet.generateFactoidPrivateKey(0, 0, 0)
 var privKey2 = wallet.generateFactoidPrivateKey(0, 0, 1)
 
+var privEcKey1 = wallet.generateEntryCreditPrivateKey(0, 0, 0)
+
 // Get Fs... private human readable address
 var humanPrivKey1 = fctUtils.keyToPrivateFctAddress(privKey1)
 var humanPrivKey2 = fctUtils.keyToPrivateFctAddress(privKey2)
 
+
 // Get FA... public human readable address
 var humanPubKey1 = fctUtils.getPublicAddress(humanPrivKey1)
 var humanPubKey2 = fctUtils.getPublicAddress(humanPrivKey2)
+
+// Get EC addresses
+var humanEcPrivKey1 = fctUtils.keyToPrivateEcAddress(privEcKey1)
+var humanEcPubKey1 = fctUtils.getPublicAddress(humanEcPrivKey1)
+
+console.log('------------------------------------------')
+console.log("FCT Address 1: m/44'/131'/0'/0/0")
 console.log(humanPubKey1)
+console.log(humanPrivKey1)
+console.log('------------------------------------------')
+console.log("FCT Address 2: m/44'/131'/0'/0/1")
 console.log(humanPubKey2)
+console.log(humanPrivKey2)
+
+console.log('------------------------------------------')
+console.log("Entry Credit : m/44'/132'/0'/0/0")
+console.log(humanEcPubKey1)
+console.log(humanEcPrivKey1)
+
+var privKeyIdentity = wallet.generateIdentityPrivateKey(0,0,0)
+var privKeyIdentity2 = wallet.generateIdentityPrivateKey(0,0,1)
+var humanPrivKeyIdentity = fctUtilsId.keyToSecretIdentityKey(privKeyIdentity)
+var humanPrivKeyIdentity2 = fctUtilsId.keyToSecretIdentityKey(privKeyIdentity2)
+var humanPubKeyIdentity = fctUtilsId.getPublicIdentityKey(humanPrivKeyIdentity)
+var humanPubKeyIdentity2 = fctUtilsId.getPublicIdentityKey(humanPrivKeyIdentity2)
+
+console.log('------------------------------------------')
+console.log("Identity1 : m/44'/143165576'/0'/0/0")
+console.log(humanPubKeyIdentity)
+console.log(humanPrivKeyIdentity)
+console.log('------------------------------------------')
+console.log("Identity2 : m/44'/143165576'/0'/0/1")
+console.log(humanPubKeyIdentity2)
+console.log(humanPrivKeyIdentity2)
+console.log('------------------------------------------')
+
+const cont = {
+   'identity-version': 1,
+   'keys': [humanPubKeyIdentity,humanPubKeyIdentity2]
+};
+
+console.log('------------------------------------------')
+console.log(cont)
+console.log('------------------------------------------')
+
+const entry = Entry.builder()
+  .extId('IdentityChain', 'utf8')
+  .extId('John', 'utf8')
+  .extId('Doe', 'utf8')
+  .content(JSON.stringify(cont),'utf8')
+  .build();
+
+const chain = new Chain(entry);
+const compose = composeChain(chain, humanEcPrivKey1);
+
+console.log('------------------COMMIT------------------')
+console.log(compose.commit.toString('hex'))
+console.log('------------------------------------------')
+console.log('------------------REVEAL------------------')
+console.log(compose.reveal.toString('hex'))
+console.log('------------------------------------------')
 
